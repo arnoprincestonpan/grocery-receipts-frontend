288f988c1ea6dd9e41611d629b07c9d0cc405489
Arno Pan <apan11@my.bcit.ca>
2025-02-03 20:38:44 -0800

    listed out item number

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,50 @@
 import './App.css'
 import receiptData from './data/receipts.json';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react';
+
+function addReceipt(){
+  console.log("Add Receipt");
+
+}
+
 
 function App() {
+
+  const [viewReceipts, setViewReceipts] = useState(false);
+
   return (
     <>
-      <div className="container mt-4">
-        <h1>Grocery Receipts</h1>
-
-        {/* Buttons Section */}
-        <div className="mb-2">
-          <div className="d-flex justify-content-between gap-2">
-            <button className="btn btn-success w-25">
-              Add
-            </button>
-            <button className="btn btn-info w-25">
-              View
-            </button>
-            <button className="btn btn-warning w-25">
-              Edit
-            </button>
-            <button className="btn btn-danger w-25">
-              Delete
+      <div className="container">
+        
+        {/* Title & Menu */}
+        <div className="container border-bottom">
+          <h1>Grocery Receipts</h1>
+          {/* Buttons Section */}
+          <div className="mb-2">
+            <div className="d-flex justify-content-between gap-2">
+              <button className="btn btn-success w-25">
+                Add
+              </button>
+              <button className="btn btn-info w-25">
+                View
+              </button>
+              <button className="btn btn-warning w-25">
+                Edit
+              </button>
+              <button className="btn btn-danger w-25">
+                Delete
+              </button>
+            </div>
+            <button className="btn btn-primary btn-lg w-100 my-2" onClick={() => setViewReceipts(!viewReceipts)}>
+              View All
             </button>
           </div>
-          <button className="btn btn-primary btn-lg w-100 my-2">
-            View All
-          </button>
         </div>
 
-        {receiptData.map((receipt, index) => (
+        {/* Receipts Section */}
+
+        {viewReceipts && receiptData.map((receipt, index) => (
           <div key={index} className="card my-2">
             <div className="card-body">
               <h2 className="card-title">
@@ -41,6 +56,7 @@ function App() {
                 <table className="table">
                   <thead>
                     <tr>
+                      <th>Item #</th>
                       <th>Name</th>
                       <th>Quantity</th>
                       <th>Price</th>
@@ -50,6 +66,7 @@ function App() {
                   <tbody>
                     {receipt.items.map((item, itemIndex) => (
                       <tr key={itemIndex}>
+                        <td>{++itemIndex}</td>
                         <td>{item.name}</td>
                         <td>{item.quantity}</td>
                         <td>${item.price.toFixed(2)}</td>
@@ -58,6 +75,7 @@ function App() {
                     ))}
                   </tbody>
                 </table>
+
                 <div className="container">
                     <div className="row">
                       <div className="col">
