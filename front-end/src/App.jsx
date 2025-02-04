5369227d5c11158c482bbe3e05f06365f816547b
Arno Pan <apan11@my.bcit.ca>
2025-02-03 16:39:59 -0800

    visualize data

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,26 +1,25 @@
 import './App.css'
-import { useState } from 'react';
-// we need to use this to manage the state of our: receipts, modal visiblity and form inputs
-import { Modal, Button, Form } from 'react-bootstrap';
-// we need modal for pop-ups, Button to trigger modal or submitting forms, form to be inside the modal
+import receiptData from './data/receipts.json';
 
 function App() {
-  // State to manage Receipts
-  const [receipts, setReceipts] = useState([]);
-
-  // State to manage modal visibility
-  const [showModal, setShowModal] = useState(false);
-
-  // State to handle form input for receipt name and date
-  const [receiptName, setReceiptName] = useState('');
-  const [receptDate, setReceiptDate] = useState('');
-
   return (
     <>
       <h1>Grocery Receipts</h1>
-      <Button variant="primary" onClick={() => setShowModal(true)}>
-        Add Receipt
-      </Button>
+      <ul>
+        {receiptData.map((receipt, index) => (
+          <li key={index}>
+            <h2>Receipt # {receipt.receiptNumber}</h2>
+            <p>Date: {receipt.date}</p>
+            <ul>
+              {receipt.items.map((item, itemIndex) => (
+                <li key={itemIndex}>
+                  {item.name} - {item.quantity} x ${item.price} - ${(item.quantity * item.price).toFixed(2)}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
