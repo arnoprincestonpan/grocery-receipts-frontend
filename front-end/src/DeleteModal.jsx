9a982d38cf38e864d3938d9d30dc3a50b2788d42
Arno Pan <apan11@my.bcit.ca>
2025-02-04 19:53:44 -0800

    add delete component start

---
+++ front-end/src/DeleteModal.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Button, Modal, Form } from 'react-boostrap';
+
+const DeleteModa = ({
+    showDeleteModal,
+    setShowDeleteModal,
+    receiptNumber,
+    setReceiptNumber,
+    checkReceiptNumber
+}) => {
+
+}
+
+export default DeleteReceiptModal;
